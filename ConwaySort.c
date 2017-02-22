d389191a23b765b121744229287fe46a97663f15
Evan Schoenberger <evanjschoenberger@gmail.com>
2017-02-22 11:19:01 -0500

    Added ConwaySort. Probably got too political.

---
+++ ConwaySort.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * ConwaySort:
+ * sorts an array by ignoring it and then
+ * printing out a new, sorted array with its
+ * own "Alternative Values."
+ * 
+ * If the new array does not appear sorted,
+ * you have been manipulated by MSM
+ */
+
+// The array to be sorted
+int[] arr = {6, 8, 3, 9, 5, 4, 1, 7, 0, 2}
+
+int main(int argc, char **argv) {
+	printf ("15, 16, 17, 18, 19, 20");
+}
